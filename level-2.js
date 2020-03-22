0b13a353dd41769565bfe7ce2dd07d2a007e7ac5
anele-jay <60442392+anele-jay@users.noreply.github.com>
2020-03-22 23:36:50 +0100

    Create level-2.js

---
+++ level-2.js
@@ -0,0 +1,15 @@
+Level 2 
+//one
+console.log(15 % 25);
+
+//two
+function innerFunction() {
+console.log("I am a function");
+} 
+outerFunction();
+
+function outerFunction() {
+console.log("I am a function");
+} 
+outerFunction();
+
